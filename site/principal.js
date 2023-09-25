5d06b187af13736b48bf39e2f563732a625cc9f1
YtBelll <ytbell@outlook.com>
2023-09-25 19:20:29 -0300

    copiando a versão atualizada

---
+++ site/principal.js
@@ -0,0 +1,7 @@
+/* Receber objetos */
+var menu = document.getElementById("menu");
+var bntAdd = document.getElementById("adicionar");
+var btnDelet = document.getElementById("deletar");
+var btnEdit = document.getElementById("editar");
+var btnRel = document.getElementById("relatorio");
+
